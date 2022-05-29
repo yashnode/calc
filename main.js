34e287c019a4b45f4e5dfda79b42be9cec8b92f2
Yash Talwar <yashtalwarwork@gmail.com>
2022-05-29 18:09:11 +0530

    added localStorage to retain theme preferences

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const checkbox = document.getElementById('checkbox');
 const buttons = document.querySelectorAll("button");
 const normalBtn = document.querySelectorAll(".normal-dark");
+if (localStorage.getItem("isDarkMode")) {
+    darkMode()
+    checkbox.checked = true
+}
 
 checkbox.addEventListener('change', () => {
+    if (!localStorage.getItem("isDarkMode")) {
+        darkMode()
+        localStorage.setItem("isDarkMode", 1);
+    } else {
+        localStorage.removeItem("isDarkMode");
+        darkMode()
+    }
+})
+
+function darkMode() {
     document.body.classList.toggle('dark-bg')
     document.querySelector("label").classList.toggle('dark-label')
     document.querySelector(".curr-val").classList.toggle('dark-curr-val')
     buttons.forEach((el) => el.classList.toggle('dark-buttons'));
     normalBtn.forEach((el) => el.classList.toggle('dark-btn'));
-})
+}
